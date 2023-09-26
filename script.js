c12fa739e3b149173a497bb824294d3fbc82cdf5
Elijah <elijahcravens46@gmail.com>
2023-09-26 11:56:56 -0600

    Created Sketch Area
    created a function that takes a size argument to create a grid of
    divs based on the size, as long as the size meets certain criteria

---
+++ script.js
@@ -0,0 +1,35 @@
+const container = document.querySelector('.container')
+
+function createElement(type, className) {
+	const element = document.createElement(type)
+
+	element.classList.add(className)
+
+	return element
+}
+
+function createSketchArea(gridSize) {
+	const sketchArea = createElement('div', 'sketch-area')
+  const sketchPad = createElement('div', 'sketchpad');
+
+	if (gridSize <= 100 && gridSize >= 16 && gridSize % 4 === 0) {
+		for (let i = 0; i < gridSize; ++i) {
+      const gridRow = createElement('div', 'grid-row');
+
+			for (let j = 0; j < gridSize; ++j) {
+				const gridItem = createElement('div', 'grid-item')
+
+				gridRow.appendChild(gridItem)
+			}
+
+      sketchPad.appendChild(gridRow);
+		}
+	} else {
+		alert('You entered an invalid sketch size. Please enter a number between 16 and 100')
+	}
+
+  sketchArea.appendChild(sketchPad);
+  container.appendChild(sketchArea);
+}
+
+createSketchArea(48)
