cda1a5dbb140b5b83faa81d6ae07cd8e5754263c
Elijah <elijahcravens46@gmail.com>
2023-09-26 14:12:12 -0600

    Refactored UI section
    refactored code so that the user can type in a number and generate
    a grid size for the number that fits the parameters.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.querySelector('.container')
+const sketchArea = createElement('div', 'sketch-area')
+const sketchPad = createElement('div', 'sketchpad')
 
 function createElement(type, className) {
 	const element = document.createElement(type)
@@ -9,58 +11,66 @@ function createElement(type, className) {
 }
 
 function createSketchArea(gridSize) {
-	const sketchArea = createElement('div', 'sketch-area')
-  const sketchPad = createElement('div', 'sketchpad');
-
-	if (gridSize <= 100 && gridSize >= 16 && gridSize % 4 === 0) {
 		for (let i = 0; i < gridSize; ++i) {
-      const gridRow = createElement('div', 'grid-row');
+			const gridRow = createElement('div', 'grid-row')
 
 			for (let j = 0; j < gridSize; ++j) {
 				const gridItem = createElement('div', 'grid-item')
 
-        gridItem.addEventListener('mouseover', function() {
-          gridItem.style.backgroundColor = '#000';
-        });
+				gridItem.addEventListener('mouseover', function () {
+					gridItem.style.backgroundColor = '#000'
+				})
 
 				gridRow.appendChild(gridItem)
 			}
 
-      sketchPad.appendChild(gridRow);
+			sketchPad.appendChild(gridRow)
 		}
-	} else {
-		alert('You entered an invalid sketch size. Please enter a number between 16 and 100')
-	}
 
-  sketchArea.appendChild(sketchPad);
-  container.appendChild(sketchArea);
+	sketchArea.appendChild(sketchPad)
+	container.appendChild(sketchArea)
 }
 
-function createUI(){
-  const uiSection = createElement('div', 'ui-section');
-  const getSize = createElement('div', 'get-grid-size');
-  const gridSizeLabel = createElement('label', 'grid-size-label');
-  const gridSizeInput = createElement('input', 'grid-size-input');
-  const setGridSize = createElement('button', 'set-size-btn');
+function createUI() {
+	const uiSection = createElement('div', 'ui-section')
+	const getSize = createElement('div', 'get-grid-size')
+	const gridSizeLabel = createElement('label', 'grid-size-label')
+	const gridSizeInput = createElement('input', 'grid-size-input')
+	const setGridSize = createElement('button', 'set-size-btn')
 
-  gridSizeLabel.setAttribute('for', 'grid-size');
-  gridSizeLabel.textContent = 'Sketch Pad Size: '
-  gridSizeInput.setAttribute('type', 'text');
-  gridSizeInput.setAttribute('id', 'grid-size');
-  setGridSize.textContent = 'Set Size';
+	gridSizeLabel.setAttribute('for', 'grid-size')
+	gridSizeLabel.textContent = 'Sketch Pad Size: '
+	gridSizeInput.setAttribute('type', 'text')
+	gridSizeInput.setAttribute('id', 'grid-size')
+	setGridSize.textContent = 'Set Size'
 
-  getSize.appendChild(gridSizeLabel);
-  getSize.appendChild(gridSizeInput);
-  getSize.appendChild(setGridSize);
+	setGridSize.addEventListener('click', changeSketchArea);
 
-  uiSection.appendChild(getSize);
+	getSize.appendChild(gridSizeLabel)
+	getSize.appendChild(gridSizeInput)
+	getSize.appendChild(setGridSize)
 
-  container.appendChild(uiSection);
+	uiSection.appendChild(getSize)
+
+	container.appendChild(uiSection)
+}
+
+function changeSketchArea(){
+  const size = document.querySelector('.grid-size-input');
+
+  if (+size.value <= 100 && +size.value >= 16 && +size.value % 4 === 0) {
+    sketchPad.innerHTML = '';
+    createSketchArea(+size.value);
+  } else if (+size.value % 4 != 0) {
+    alert('Please enter a number that is divisible by 4');
+  } else {
+    alert('You entered an invalid sketch size. Please enter a number between 16 and 100')
+  }
 }
 
 function sketch() {
-  createSketchArea(16)
-  createUI();
+  createUI()
+	createSketchArea(16)
 }
 
-sketch();
+sketch()
